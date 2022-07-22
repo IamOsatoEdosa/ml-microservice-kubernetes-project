028511c66d43ef9137fb221268d7801b5b8c036c
EC2 Default User <ec2-user@ip-172-31-42-85.ec2.internal>
2022-07-22 13:21:53 +0000

    Done with output files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="osatoedosa/predictionapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictionapp --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predictionapp 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="osatoedosa/predictionapp"
 
 # Step 2:  
 # Authenticate & tag
+docker login -u osatoedosa
+docker tag predictionapp:v.1.0 ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
